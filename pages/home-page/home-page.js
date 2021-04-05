cd3d6ed5f83c991bf9b7f7d6eb8e5e0b7fe8cd59
LuanPoi (Ubuntu) <luan_poi@hotmail.com>
2021-04-05 13:13:33 -0300

    Real first commit

---
+++ pages/home-page/home-page.js
@@ -0,0 +1,9 @@
+// function test(){
+//     var scrolled = $(window).scrollTop();
+//     $('.profile-picture').css('opacity',(100-(scrolled*0.2))+'%');
+//     console.log(scrolled);
+// }
+
+// $(document).scroll(function(e){
+//     test();
+// });
